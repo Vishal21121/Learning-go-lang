09fd85814da137fe7db30f87130ae4694dbb41b4
Vishal21121 <vs84077000@gmail.com>
2022-12-25 20:17:18 +0530

    added code for reponse of course based on request id

--- 24-buildapi/main.go
+++ 24-buildapi/main.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 // Model for the course - file
@@ -41,4 +43,23 @@ func getAllCourses(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Get all courses")
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(courses)
+}
+
+func getOneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Get one course")
+	w.Header().Set("Content-Type", "application/json")
+
+	// grab id from request
+	params := mux.Vars(r)
+	fmt.Println(params)
+
+	// loop through courses, find matching id and return the response
+	for _, course := range courses {
+		if course.CourseID == params["id"] {
+			json.NewEncoder(w).Encode(course)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode("Now course found with the given id")
+	return
 }
