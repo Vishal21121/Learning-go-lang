6c3e07152f42e0bf9b3435d552717e8260ff6034
Vishal21121 <vs84077000@gmail.com>
2023-12-27 09:13:34 +0530

    added: go routines file and made some changes in rest files

---
+++ 26-goroutines/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"sync"
+)
+
+var wg sync.WaitGroup
+
+func main() {
+	// go fires the thead which is responsible for executing the greeter method with Hello as paramter. But we never told it to come back or we never waited for it to come back.
+	//* go greeter("Hello")
+	//* greeter("World")
+
+	websiteList := []string{
+		"https://lco.dev",
+		"https://go.dev",
+		"https://google.com",
+		"https://fb.com",
+		"https://github.com",
+	}
+
+	for _, web := range websiteList {
+		go getStatusCode(web)
+		// wg adds the go routines which are out there right now
+		wg.Add(1)
+	}
+	// it prevents the main function from exiting till all the go rountines have done their execution.
+	wg.Wait()
+}
+
+// func greeter(str string) {
+// 	for i := 0; i < 5; i++ {
+//* 		// here we are waiting so that therad returns, but this is not the ideal way we have package named as sync to handle it.
+// 		time.Sleep(1 * time.Second)
+// 		fmt.Println(str)
+// 	}
+// }
+
+func getStatusCode(endpoint string) {
+	// it indicates the completion of the go routine and it should run at the end hence defer keyword is used.
+	defer wg.Done()
+	response, err := http.Get(endpoint)
+	if err != nil {
+		fmt.Println("OOPS in endpoint")
+	}
+	fmt.Printf("%d status code for %s\n", response.StatusCode, endpoint)
+}
